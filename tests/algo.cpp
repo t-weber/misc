f70f78ea54a5d3dcf0466485a8974abc88975339
Tobias Weber <tobias.weber@tum.de>
2017-11-11 17:00:46 +0100

    further cpp17 tests

---
+++ tests/algo.cpp
@@ -0,0 +1,77 @@
+/**
+ * path/file operations
+ * @author Tobias Weber
+ * @date 11-nov-17
+ *
+ * gcc -o algo algo.cpp -std=c++17 -lstdc++ -lstdc++fs
+ */
+
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <random>
+
+
+int main()
+{
+	// --------------------------------------------------------------------
+	// misc
+	std::cout << "gcd(10, 4) = " << std::gcd(10,4) << std::endl;
+	std::cout << "lcm(10, 4) = " << std::lcm(10,4) << std::endl;
+
+	std::cout
+		<< std::clamp(10, 0, 5, [](auto a, auto b) -> bool { return a<b; })
+		<< std::endl;
+	// --------------------------------------------------------------------
+
+
+
+	// --------------------------------------------------------------------
+	// accumulate / reduce
+	using T = int;
+	std::vector<T> v{{1,2,3,4,5}};
+	T init = 1;
+
+	auto op_red = [](T a, T b) -> T
+	{
+		return a*b;
+	};
+
+	T sum = std::accumulate(v.begin(), v.end(), init, op_red);
+	std::cout << sum << std::endl;
+
+	T sum2 = std::reduce(std::execution::par/*seq*/, v.begin(), v.end(), init, op_red);
+	std::cout << sum2 << std::endl;
+	// --------------------------------------------------------------------
+
+
+
+	// --------------------------------------------------------------------
+	// transform
+	auto op_trafo = [](T a) -> T
+	{
+		return a*2;
+	};
+
+	std::vector w(v.size(), 0);
+	std::transform(v.begin(), v.end(), w.begin(), op_trafo);
+	T sum3 = std::accumulate(w.begin(), w.end(), init, op_red);
+	std::cout << sum3 << std::endl;
+
+	T sum4 = std::transform_reduce(std::execution::par, v.begin(), v.end(), init, op_red, op_trafo);
+	std::cout << sum4 << std::endl;
+	// --------------------------------------------------------------------
+
+
+
+	// --------------------------------------------------------------------
+	// sample
+	std::mt19937 rndeng(123);
+	std::sample(v.begin(), v.end(), std::ostream_iterator<T>(std::cout, " "), 3, rndeng);
+	std::cout << std::endl;
+	// --------------------------------------------------------------------
+
+	return 0;
+}
---
+++ tests/fs.cpp
@@ -0,0 +1,64 @@
+/**
+ * path/file operations
+ * @author Tobias Weber
+ * @date 11-nov-17
+ *
+ * gcc -o fs fs.cpp -std=c++14 -lstdc++ -lstdc++fs
+ * gcc -o fs fs.cpp -std=c++14 -lstdc++ -lboost_filesystem -lboost_system
+ */
+
+#include <iostream>
+
+#include <experimental/filesystem>
+//#include <boost/filesystem.hpp>
+
+namespace fs = std::experimental::filesystem;
+//namespace fs = boost::filesystem;
+
+
+void iterate_dir(const fs::path& path)
+{
+	using t_diriter = fs::directory_iterator;
+	//using t_diriter = fs::recursive_directory_iterator;
+
+	t_diriter diriter(path);
+	for(t_diriter iter=fs::begin(diriter); iter!=fs::end(diriter); ++iter)
+	{
+		const fs::directory_entry& ent = *iter;
+		const fs::path& entpath = ent;
+
+
+		// ------------------------------------------------------------
+		// example:
+
+		//std::cout << "dir depth: " << iter.depth() << ", ";
+
+		fs::path file = entpath.filename();
+		fs::path file_noext = entpath.stem();
+		fs::path ext = entpath.extension();
+		fs::path parent = entpath.parent_path();
+
+		if(fs::is_regular_file(entpath))
+			std::cout << file << ": " << fs::file_size(entpath) << std::endl;
+		else if(fs::is_directory(entpath))
+			std::cout << entpath << std::endl;
+		// ------------------------------------------------------------
+	}
+}
+
+
+int main()
+{
+	std::cout << "Path char size: " << sizeof(fs::path::value_type) << std::endl;
+
+	fs::path pathTmp = fs::temp_directory_path();
+	if(fs::exists(pathTmp))
+		std::cout << "tmp: " << pathTmp << std::endl;
+
+	fs::path path = fs::current_path();
+	std::cout << "cwd: " << path << std::endl;
+
+	iterate_dir(path.parent_path());
+
+	return 0;
+}
