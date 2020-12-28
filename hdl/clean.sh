2ed4d7dde33e79bca306ebc2cba58ede42f7e489
Tobias Weber <tobias.weber@tum.de>
2020-12-28 09:27:11 +0100

    hdl: reordered files

--- hdl/clean.sh
+++ hdl/clean.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+#
+# @author Tobias Weber <tobias.weber@tum.de>
+# @date dec-2020
+# @license see 'LICENSE.EUPL' file
+#
 
-rm -fv *.o
-rm -fv testbed-thetester
-rm -fv *.cf
-rm -fv *.vcd
+dirs=( "." "lib" "test" "calc" )
+
+for dir in "${dirs[@]}"; do
+	pushd "${dir}"
+
+	rm -fv *.o
+	rm -fv testbed-thetester
+	rm -fv *.cf
+	rm -fv *.vcd
+
+	popd
+done
