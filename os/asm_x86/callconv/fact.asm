df362771e448a91cb2af1608b06fbf4c7b42bc5a
Tobias Weber <tobias.weber@tum.de>
2021-04-06 02:06:59 +0200

    continued with calling conventions test

--- os/asm_x86/callconv/fact.asm
+++ os/asm_x86/callconv/fact.asm
@@ -3,6 +3,8 @@
 ; @date apr-2021
 ; @license see 'LICENSE.GPL' file
 ;
+; @see https://en.wikipedia.org/wiki/X86_calling_conventions
+;
 
 %define WORD_SIZE 4
 %define STACK_SIZE 1*WORD_SIZE                    ; size of local stack frame
@@ -10,12 +12,17 @@
 
 section .text
 global fact_cdecl_asm
+global fact_stdcall_asm
+global fact_fastcall_asm
+
 extern fact_cdecl_c
+extern fact_stdcall_c
+extern fact_fastcall_c
 
 
 ;
 ; calculate factorials
-; dword argument on stack
+; dword arguments on stack
 ; returns result in eax
 ;
 fact_cdecl_asm:
@@ -24,7 +31,7 @@ fact_cdecl_asm:
 	sub esp, STACK_SIZE                       ; create local var stack frame
 
 	;pusha
-	push ebx                                  ; save registers that are changed
+	push ecx                                  ; save registers that are changed
 	push edx
 
 
@@ -35,13 +42,13 @@ fact_cdecl_asm:
 	mov [ebp - WORD_SIZE*1], eax              ; save number as local var
 	dec eax                                   ; --num
 
-	mov ebx, [ebp + WORD_SIZE + WORD_SIZE*2]  ; get function selector arg
+	mov ecx, [ebp + WORD_SIZE + WORD_SIZE*2]  ; get function selector arg
 
 
-	push ebx                                  ; push pure_call arg
+	push ecx                                  ; push pure_call arg
 	push eax                                  ; push num arg
 
-	cmp ebx, 1                                ; pure_call != 1?
+	cmp ecx, 1                                ; pure_call != 1?
 	jnz .c_call
 		call fact_cdecl_asm               ; call asm version of this function
 		jmp .c_call_end
@@ -62,12 +69,125 @@ fact_cdecl_asm:
 
 	.end:
 	pop edx
-	pop ebx                                   ; restore registers that were changed
+	pop ecx                                   ; restore registers that were changed
 	;popa
 
 	mov esp, ebp                              ; remove local var stack frame
 	pop ebp
 	ret
+
+
+
+;
+; calculate factorials
+; dword arguments on stack
+; returns result in eax
+;
+fact_stdcall_asm:
+	push ebp
+	mov ebp, esp
+	sub esp, STACK_SIZE                       ; create local var stack frame
+
+	;pusha
+	push ecx                                  ; save registers that are changed
+	push edx
+
+
+	mov eax, [ebp + WORD_SIZE + WORD_SIZE*1]  ; get number arg
+	cmp eax, 1                                ; recursion end condition
+	jle .end_le1                              ; num <= 1?
+
+	mov [ebp - WORD_SIZE*1], eax              ; save number as local var
+	dec eax                                   ; --num
+
+	mov ecx, [ebp + WORD_SIZE + WORD_SIZE*2]  ; get function selector arg
+
+
+	push ecx                                  ; push pure_call arg
+	push eax                                  ; push num arg
+
+	cmp ecx, 1                                ; pure_call != 1?
+	jnz .c_call
+		call fact_stdcall_asm             ; call asm version of this function
+		jmp .c_call_end
+	.c_call:
+		call fact_stdcall_c               ; call c version of this function
+	.c_call_end:
+
+
+	mov edx, [ebp - WORD_SIZE*1]              ; get local var
+	mul edx                                   ; multiply with result
+	jmp .end
+
+	.end_le1:                                 ; num <= 1?
+	mov eax, 1
+
+	.end:
+	pop edx
+	pop ecx                                   ; restore registers that were changed
+	;popa
+
+	mov esp, ebp                              ; remove local var stack frame
+	pop ebp
+
+	;add esp, WORD_SIZE*2                     ; remove args from stack
+	ret WORD_SIZE*2
+
+
+
+;
+; calculate factorials
+; dword arguments in ecx (number) and edx (pure_call)
+; returns result in eax
+;
+fact_fastcall_asm:
+	push ebp
+	mov ebp, esp
+	sub esp, WORD_SIZE*3                      ; create local var stack frame
+
+	;pusha
+	push ecx                                  ; save registers that are changed
+	push edx
+
+	mov [ebp - WORD_SIZE*2], ecx              ; save arg 1 (number) as local var
+	mov [ebp - WORD_SIZE*3], edx              ; save arg 2 (pure_call) as local var
+
+	mov eax, [ebp - WORD_SIZE*2]              ; get number arg
+	cmp eax, 1                                ; recursion end condition
+	jle .end_le1                              ; num <= 1?
+
+	mov [ebp - WORD_SIZE*1], eax              ; save number as local var
+	dec eax                                   ; --num
+
+	mov ecx, eax                              ; arg 1
+	mov edx, [ebp - WORD_SIZE*3]              ; arg 2
+
+	cmp edx, 1                                ; pure_call != 1?
+	jnz .c_call
+		call fact_fastcall_asm            ; call asm version of this function
+		jmp .c_call_end
+	.c_call:
+		call fact_fastcall_c              ; call c version of this function
+	.c_call_end:
+
+
+	mov ecx, [ebp - WORD_SIZE*1]              ; get local var
+	mul ecx                                   ; multiply with result
+	jmp .end
+
+	.end_le1:                                 ; num <= 1?
+	mov eax, 1
+
+	.end:
+	pop edx
+	pop ecx                                   ; restore registers that were changed
+	;popa
+
+	mov esp, ebp                              ; remove local var stack frame
+	pop ebp
+
+	;add esp, WORD_SIZE*0                     ; remove args from stack
+	ret WORD_SIZE*0
 
 
 section .data
--- os/asm_x86/callconv/main.c
+++ os/asm_x86/callconv/main.c
@@ -11,6 +11,8 @@ typedef unsigned int t_int;
 
 
 extern t_int __attribute__((__cdecl__)) fact_cdecl_asm(t_int num, t_int pure_call);
+extern t_int __attribute__((__stdcall__)) fact_stdcall_asm(t_int num, t_int pure_call);
+extern t_int __attribute__((__fastcall__)) fact_fastcall_asm(t_int num, t_int pure_call);
 
 
 t_int __attribute__((__cdecl__)) fact_cdecl_c(t_int num, t_int pure_call)
@@ -28,18 +30,77 @@ t_int __attribute__((__cdecl__)) fact_cdecl_c(t_int num, t_int pure_call)
 }
 
 
+t_int __attribute__((__stdcall__)) fact_stdcall_c(t_int num, t_int pure_call)
+{
+	if(num < 1)
+		return 1;
+	if(num == 2)
+		return 2;
+
+	// call c or asm version of this function
+	if(pure_call)
+		return num * fact_stdcall_c(num-1, pure_call);
+	else
+		return num * fact_stdcall_asm(num-1, pure_call);
+}
+
+
+t_int __attribute__((__fastcall__)) fact_fastcall_c(t_int num, t_int pure_call)
+{
+	if(num < 1)
+		return 1;
+	if(num == 2)
+		return 2;
+
+	// call c or asm version of this function
+	if(pure_call)
+		return num * fact_fastcall_c(num-1, pure_call);
+	else
+		return num * fact_fastcall_asm(num-1, pure_call);
+}
+
+
 int main()
 {
+	fprintf(stdout, "cdecl\n");
 	for(t_int num=0; num<12; ++num)
 	{
 		t_int res1 = fact_cdecl_c(num, 1);
 		t_int res2 = fact_cdecl_asm(num, 1);
 		t_int res3 = fact_cdecl_c(num, 0);
 
-		fprintf(stdout, "c function          : %d! = %d\n", num, res1);
-		fprintf(stdout, "asm function        : %d! = %d\n", num, res2);
-		fprintf(stdout, "c/asm mixed function: %d! = %d\n", num, res3);
-		fprintf(stdout, "\n");
+		fprintf(stdout, "\tc function          : %d! = %d\n", num, res1);
+		fprintf(stdout, "\tasm function        : %d! = %d\n", num, res2);
+		fprintf(stdout, "\tc/asm mixed function: %d! = %d\n", num, res3);
+		fprintf(stdout, "\t\n");
+	}
+
+
+	fprintf(stdout, "stdcall\n");
+	for(t_int num=0; num<12; ++num)
+	{
+		t_int res1 = fact_stdcall_c(num, 1);
+		t_int res2 = fact_stdcall_asm(num, 1);
+		t_int res3 = fact_stdcall_c(num, 0);
+
+		fprintf(stdout, "\tc function          : %d! = %d\n", num, res1);
+		fprintf(stdout, "\tasm function        : %d! = %d\n", num, res2);
+		fprintf(stdout, "\tc/asm mixed function: %d! = %d\n", num, res3);
+		fprintf(stdout, "\t\n");
+	}
+
+
+	fprintf(stdout, "fastcall\n");
+	for(t_int num=0; num<12; ++num)
+	{
+		t_int res1 = fact_fastcall_c(num, 1);
+		t_int res2 = fact_fastcall_asm(num, 1);
+		t_int res3 = fact_fastcall_c(num, 0);
+
+		fprintf(stdout, "\tc function          : %d! = %d\n", num, res1);
+		fprintf(stdout, "\tasm function        : %d! = %d\n", num, res2);
+		fprintf(stdout, "\tc/asm mixed function: %d! = %d\n", num, res3);
+		fprintf(stdout, "\t\n");
 	}
 
 	return 0;
