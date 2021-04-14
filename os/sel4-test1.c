26237b897ef2a32dbe1bc56c37c19dc260198081
Tobias Weber <tobias.weber@tum.de>
2021-04-15 00:39:42 +0200

    continued with sel4 test

--- os/sel4-test1.c
+++ os/sel4-test1.c
@@ -23,15 +23,15 @@
 	#define printf(...) {}
 #endif
 
-typedef seL4_Uint8  u8;
-typedef seL4_Int8   i8;
-typedef seL4_Uint16 u16;
-typedef seL4_Int16  i16;
-typedef seL4_Uint32 u32;
-typedef seL4_Int32  i32;
-typedef seL4_Uint64 u64;
-typedef seL4_Int64  i64;
-typedef seL4_Word   word_t;
+typedef seL4_Uint8       u8;
+typedef seL4_Int8        i8;
+typedef seL4_Uint16      u16;
+typedef seL4_Int16       i16;
+typedef seL4_Uint32      u32;
+typedef seL4_Int32       i32;
+typedef seL4_Uint64      u64;
+typedef seL4_Int64       i64;
+typedef seL4_Word        word_t;
 
 #define SCREEN_ROW_SIZE  25
 #define SCREEN_COL_SIZE  80
@@ -185,9 +185,10 @@ u64 fibo(u64 num)
 }
 
 
-void calc(u64 num_start, u64 num_end, i8 *charout)
+void calc(u64 num_end, i8 *charout, seL4_SlotPos notify)
 {
 	printf("Start of calc() thread.\n");
+	const u64 num_start = 0;
 	const u64 spacing = 16;
 
 	my_memset(charout, 0, SCREEN_SIZE*2);
@@ -213,6 +214,8 @@ void calc(u64 num_start, u64 num_end, i8 *charout)
 	}
 
 	printf("End of calc() thread.\n");
+
+	seL4_Signal(notify);
 	while(1) seL4_Yield();
 }
 
@@ -231,7 +234,7 @@ seL4_SlotPos find_untyped(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end,
 			continue;
 
 		word_t cur_size = (1<< cur_descr->sizeBits);
-		//printf("Untyped slot %lx: size=%ld, physical address=0x%lx.\n",
+		//printf("Untyped slot 0x%lx: size=%ld, physical address=0x%lx.\n",
 		//	cur_slot, cur_size, cur_descr->paddr);
 		if(cur_size < needed_size)
 			continue;
@@ -259,7 +262,7 @@ seL4_SlotPos find_devicemem(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end
 		word_t addr_start = cur_descr->paddr;
 		word_t addr_end = addr_start + size;
 
-		//printf("Untyped device slot %lx: physical addresses=[0x%lx..0x%lx[.\n",
+		//printf("Untyped device slot 0x%lx: physical addresses=[0x%lx..0x%lx[.\n",
 		//	cur_slot, cur_descr->paddr, addr_end);
 		if(addr >= addr_start && addr < addr_end)
 			return cur_slot;
@@ -269,74 +272,9 @@ seL4_SlotPos find_devicemem(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end
 }
 
 
-seL4_SlotPos map_page(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end,
+void map_pagetables(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end,
 	const seL4_UntypedDesc* untyped_list, seL4_SlotPos* cur_slot,
 	word_t virt_addr)
-{
-	const seL4_SlotPos cnode = seL4_CapInitThreadCNode;
-	const seL4_SlotPos vspace = seL4_CapInitThreadVSpace;
-	seL4_X86_VMAttributes vmattr = seL4_X86_Default_VMAttributes;
-
-	/*seL4_SlotPos table_slot = find_untyped(untyped_start, untyped_end,
-		untyped_list, PAGE_SIZE*1024);
-	if(table_slot < untyped_start)
-		printf("Error: No large enough untyped slot found!\n");
-	printf("Loading tables into untyped slot 0x%lx.\n", table_slot);
-
-	// load three levels of page tables
-	const seL4_ArchObjectType pagetable_objs[] =
-	{
-		seL4_X86_PDPTObject,
-		seL4_X86_PageDirectoryObject,
-		seL4_X86_PageTableObject
-	};
-
-	seL4_Error (*pagetable_map[])(word_t, seL4_CPtr, word_t, seL4_X86_VMAttributes) =
-	{
-		&seL4_X86_PDPT_Map,
-		&seL4_X86_PageDirectory_Map,
-		&seL4_X86_PageTable_Map
-	};
-
-	seL4_SlotPos pagetable_slots[] = { 0, 0, 0 };
-
-	for(i16 level=0; level<sizeof(pagetable_objs)/sizeof(pagetable_objs[0]); ++level)
-	{
-		pagetable_slots[level] = (*cur_slot)++;
-		seL4_Untyped_Retype(table_slot, pagetable_objs[level], 0, cnode,
-			0, 0, pagetable_slots[level], 1);
-		if((*pagetable_map[level])(pagetable_slots[level],
-			vspace, virt_addr, vmattr) != seL4_NoError)
-		{
-			printf("Error mapping page table level %d!\n", level);
-			break;
-		}
-	}*/
-
-
-	seL4_SlotPos base_slot = find_untyped(untyped_start, untyped_end, untyped_list, PAGE_SIZE);
-	printf("Using device memory slot 0x%lx.\n", base_slot);
-
-	seL4_SlotPos page_slot = (*cur_slot)++;
-	seL4_Untyped_Retype(base_slot, PAGE_TYPE, 0, cnode, 0, 0, page_slot, 1);
-
-	if(seL4_X86_Page_Map(page_slot, vspace, virt_addr, /*seL4_ReadWrite*/seL4_AllRights, vmattr)
-		!= seL4_NoError)
-	{
-		printf("Error mapping page!\n");
-	}
-
-	seL4_X86_Page_GetAddress_t addr_info = seL4_X86_Page_GetAddress(page_slot);
-	printf("Mapped virtual address: 0x%lx -> physical address: 0x%lx.\n",
-		virt_addr, addr_info.paddr);
-
-	return page_slot;
-}
-
-
-seL4_SlotPos map_page_phys(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end,
-	const seL4_UntypedDesc* untyped_list, seL4_SlotPos* cur_slot,
-	word_t virt_addr, word_t phys_addr)
 {
 	const seL4_SlotPos cnode = seL4_CapInitThreadCNode;
 	const seL4_SlotPos vspace = seL4_CapInitThreadVSpace;
@@ -377,7 +315,44 @@ seL4_SlotPos map_page_phys(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end,
 			break;
 		}
 	}
+}
 
+
+seL4_SlotPos map_page(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end,
+	const seL4_UntypedDesc* untyped_list, seL4_SlotPos* cur_slot,
+	word_t virt_addr)
+{
+	const seL4_SlotPos cnode = seL4_CapInitThreadCNode;
+	const seL4_SlotPos vspace = seL4_CapInitThreadVSpace;
+	seL4_X86_VMAttributes vmattr = seL4_X86_Default_VMAttributes;
+
+	seL4_SlotPos base_slot = find_untyped(untyped_start, untyped_end, untyped_list, PAGE_SIZE);
+	printf("Using device memory slot 0x%lx.\n", base_slot);
+
+	seL4_SlotPos page_slot = (*cur_slot)++;
+	seL4_Untyped_Retype(base_slot, PAGE_TYPE, 0, cnode, 0, 0, page_slot, 1);
+
+	if(seL4_X86_Page_Map(page_slot, vspace, virt_addr, seL4_AllRights, vmattr)
+		!= seL4_NoError)
+	{
+		printf("Error mapping page!\n");
+	}
+
+	seL4_X86_Page_GetAddress_t addr_info = seL4_X86_Page_GetAddress(page_slot);
+	printf("Mapped virtual address: 0x%lx -> physical address: 0x%lx.\n",
+		virt_addr, addr_info.paddr);
+
+	return page_slot;
+}
+
+
+seL4_SlotPos map_page_phys(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end,
+	const seL4_UntypedDesc* untyped_list, seL4_SlotPos* cur_slot,
+	word_t virt_addr, word_t phys_addr)
+{
+	const seL4_SlotPos cnode = seL4_CapInitThreadCNode;
+	const seL4_SlotPos vspace = seL4_CapInitThreadVSpace;
+	seL4_X86_VMAttributes vmattr = seL4_X86_Default_VMAttributes;
 
 	seL4_SlotPos base_slot = find_devicemem(untyped_start, untyped_end,
 		untyped_list, (word_t)phys_addr);
@@ -409,6 +384,18 @@ seL4_SlotPos map_page_phys(seL4_SlotPos untyped_start, seL4_SlotPos untyped_end,
 }
 
 
+seL4_SlotPos get_slot(word_t obj, word_t obj_size, 
+	seL4_SlotPos untyped_start, seL4_SlotPos untyped_end, const seL4_UntypedDesc* untyped_list, 
+	seL4_SlotPos* cur_slot, seL4_SlotPos cnode)
+{
+	seL4_SlotPos slot = find_untyped(untyped_start, untyped_end, untyped_list, obj_size);
+	seL4_SlotPos offs = (*cur_slot)++;
+	seL4_Untyped_Retype(slot, obj, 0, cnode, 0, 0, offs, 1);
+
+	return offs;
+}
+
+
 i64 main()
 {
 	printf("--------------------------------------------------------------------------------\n");
@@ -433,22 +420,25 @@ i64 main()
 	seL4_SlotPos cur_slot = empty_start;
 	seL4_SlotPos cur_untyped_slot = untyped_start;
 
-	// some (arbitrary) virtual address to map the video/character memory to
-	word_t virt_addr_char = 0x8000000000;
+	// (arbitrary) virtual addresses to map page tables, video ram and the TCB stack into
+	word_t virt_addr_tables = 0x8000000000;
+	word_t virt_addr_char = 0x8000001000;
+	word_t virt_addr_tcb_stack = 0x8000002000;
+
+	// map the page tables
+	map_pagetables(untyped_start, untyped_end, untyped_list, &cur_slot, virt_addr_tables);
+
 	// find page whose frame contains the vga memory
 	seL4_SlotPos page_slot = map_page_phys(untyped_start, untyped_end,
 		untyped_list, &cur_slot, virt_addr_char, CHAROUT_PHYS);
 
 	// run calculation in another thread
 	// create a page frame for the thread's stack
-	word_t virt_addr_tcb_stack = 0x8000001000;
 	seL4_SlotPos page_slot_tcb_stack = map_page(untyped_start, untyped_end,
 		untyped_list, &cur_slot, virt_addr_tcb_stack);
 
-	seL4_SlotPos tcb_slot = find_untyped(untyped_start, untyped_end,
-		untyped_list, 1<<seL4_TCBBits);
-	seL4_SlotPos tcb = cur_slot++;
-	seL4_Untyped_Retype(tcb_slot, seL4_TCBObject, 0, this_cnode, 0, 0, tcb, 1);
+	seL4_SlotPos tcb = get_slot(seL4_TCBObject, 1<<seL4_TCBBits, 
+		untyped_start, untyped_end, untyped_list, &cur_slot, this_cnode);
 
 	// the child thread uses the main thread's cnode and vspace
 	if(seL4_TCB_SetSpace(tcb, 0, this_cnode, 0, this_vspace, 0) != seL4_NoError)
@@ -457,6 +447,16 @@ i64 main()
 	// doesn't seem to get scheduled otherwise...
 	if(seL4_TCB_SetPriority(tcb, this_tcb, seL4_MaxPrio) != seL4_NoError)
 		printf("Error: Cannot set TCB priority!\n");
+
+	// create semaphore for thread signalling
+	seL4_SlotPos notify = get_slot(seL4_NotificationObject, 1<<seL4_NotificationBits, 
+		untyped_start, untyped_end, untyped_list, &cur_slot, this_cnode);
+
+	// copy semaphore
+	seL4_SlotPos notify2 = cur_slot++;
+	if(seL4_CNode_Mint(this_cnode, notify2, seL4_WordBits, this_cnode, 
+		notify, seL4_WordBits, seL4_AllRights, 0) != seL4_NoError)
+		printf("Error: Mint failed.");
 
 	seL4_UserContext tcb_context;
 	i32 num_regs = sizeof(tcb_context)/sizeof(tcb_context.rax);
@@ -467,11 +467,11 @@ i64 main()
 	tcb_context.rip = (word_t)&calc;          // entry point
 	tcb_context.rsp = (word_t)(virt_addr_tcb_stack + PAGE_SIZE);  // stack
 	tcb_context.rbp = (word_t)(virt_addr_tcb_stack + PAGE_SIZE);  // stack
-	tcb_context.rdi = 0;                      // arg 1
-	tcb_context.rsi = 12;                     // arg 2
-	tcb_context.rdx = (word_t)virt_addr_char; // arg 3
+	tcb_context.rdi = 12;                     // arg 1: number of calculations
+	tcb_context.rsi = (word_t)virt_addr_char; // arg 2: vga ram
+	tcb_context.rdx = (word_t)notify;         // arg 3: semaphore
 
-	printf("rip = %lx, rsp = %lx, rflags = %lx, rdi = %lx, rsi = %lx, rdx = %lx\n",
+	printf("rip = 0x%lx, rsp = 0x%lx, rflags = 0x%lx, rdi = 0x%lx, rsi = 0x%lx, rdx = 0x%lx.\n",
 		tcb_context.rip, tcb_context.rsp, tcb_context.rflags,
 		tcb_context.rdi, tcb_context.rsi, tcb_context.rdx);
 
@@ -479,13 +479,19 @@ i64 main()
 	if(seL4_TCB_WriteRegisters(tcb, 1, 0, num_regs, &tcb_context) != seL4_NoError)
 		printf("Error writing TCB registers!\n");
 
+	printf("Waiting for thread to end.");
+	seL4_Wait(notify2, 0);
+	printf("Thread ended.\n");
+
 	// end program
-	//seL4_CNode_Revoke(cnode, tcb_slot, seL4_TCBBits);
-	//seL4_CNode_Revoke(cnode, page_slot, seL4_WordBits);
-	//seL4_CNode_Revoke(cnode, base_slot, seL4_WordBits);
-	//seL4_CNode_Revoke(cnode, table_slot, seL4_WordBits);
+	seL4_TCB_Suspend(tcb);
+	seL4_CNode_Revoke(this_cnode, page_slot_tcb_stack, seL4_WordBits);
+	seL4_CNode_Revoke(this_cnode, page_slot, seL4_WordBits);
+	//seL4_CNode_Revoke(this_cnode, base_slot, seL4_WordBits);
+	//seL4_CNode_Revoke(this_cnode, table_slot, seL4_WordBits);
 
 	printf("--------------------------------------------------------------------------------\n");
 	while(1) seL4_Yield();
 	return 0;
 }
+
