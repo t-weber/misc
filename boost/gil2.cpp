bbca59f32211321ca93d70793a141cd4fac994d8
Tobias Weber <tobias.weber@tum.de>
2022-01-22 04:29:46 +0100

    drawing tests

--- boost/gil2.cpp
+++ boost/gil2.cpp
@@ -11,6 +11,7 @@
  */
 
 #include <iostream>
+#include <cmath>
 
 #include <boost/gil/image.hpp>
 #include <boost/gil/extension/io/png.hpp>
@@ -26,50 +27,72 @@ template<class t_view, class t_col = typename t_view::value_type,
 	class t_x = typename t_view::x_coord_t, class t_y = typename t_view::y_coord_t>
 void draw_line(t_view& view, t_x x_start, t_y y_start, t_x x_end, t_y y_end, t_col col = t_col{0x00})
 {
+	//std::cout << "(" << x_start << ", " << y_start << ") -> (" << x_end << ", " << y_end << ")" << std::endl;
+
 	const t_x x_range = x_end - x_start;
 	const t_y y_range = y_end - y_start;
+	const t_x x_range_abs = std::abs(x_range);
+	const t_y y_range_abs = std::abs(y_range);
 
 	const t_x x_inc = x_range > 0 ? 1 : -1;
 	const t_y y_inc = y_range > 0 ? 1 : -1;
 
 
+	auto x_in_bounds = [x_end, x_inc](t_x x) -> bool
+	{
+		if(x_inc > 0)
+			return x <= x_end;
+		return x >= x_end;
+	};
+
+	auto y_in_bounds = [y_end, y_inc](t_y y) -> bool
+	{
+		if(y_inc > 0)
+			return y <= y_end;
+		return y >= y_end;
+	};
+
+
 	// special cases: straight line
 	if(x_range == 0)
 	{
-		for(t_y y=y_start; y!=y_end; y+=y_inc)
+		for(t_y y=y_start; y_in_bounds(y); y+=y_inc)
 			view(x_start, y) = col;
 		return;
 	}
 
 	if(y_range == 0)
 	{
-		for(t_x x=x_start; x!=x_end; x+=x_inc)
+		for(t_x x=x_start; x_in_bounds(x); x+=x_inc)
 			view(x, y_start) = col;
 		return;
 	}
 
 
 	// general case: sloped line with x range larger than y range
-	if(x_range >= y_range)
+	if(x_range_abs >= y_range_abs)
 	{
 		const t_x mult = 2;
 		t_y y = y_start;
 		t_x err = x_range * mult/2;
 
-		for(t_x x=x_start; x!=x_end; x+=x_inc)
+		for(t_x x=x_start; x_in_bounds(x); x+=x_inc)
 		{
-			view(x, y) = col;
-			err -= mult*y_range;
+			//std::cout << "x=" << x << ", y=" << y << ", err=" << err << std::endl;
 
-			if(err < 0)
+			view(x, y) = col;
+			err -= mult * y_range;
+
+			if(err < 0 && y_inc > 0)
 			{
-				++y;
-				err += mult*x_range;
+				y += y_inc;
+				err += mult * x_range_abs;
 			}
-			else if(err > 0)
+
+			if(err > 0 && y_inc < 0)
 			{
-				--y;
-				err -= mult*x_range;
+				y += y_inc;
+				err -= mult * x_range_abs;
 			}
 		}
 	}
@@ -82,22 +105,85 @@ void draw_line(t_view& view, t_x x_start, t_y y_start, t_x x_end, t_y y_end, t_c
 		t_x x = x_start;
 		t_y err = y_range * mult/2;
 
-		for(t_y y=y_start; y!=y_end; y+=y_inc)
+		for(t_y y=y_start; y_in_bounds(y); y+=y_inc)
 		{
-			view(x, y) = col;
-			err -= mult*x_range;
+			//std::cout << "x=" << x << ", y=" << y << ", err=" << err << std::endl;
 
-			if(err < 0)
+			view(x, y) = col;
+			err -= mult * x_range;
+
+			if(err < 0 && x_inc > 0)
 			{
-				++x;
-				err += mult*y_range;
+				x += x_inc;
+				err += mult * y_range_abs;
 			}
-			else if(err > 0)
+
+			else if(err > 0 && x_inc < 0)
 			{
-				--x;
-				err -= mult*y_range;
+				x += x_inc;
+				err -= mult * y_range_abs;
 			}
 		}
+	}
+}
+
+
+/**
+ * bresenham algorithm
+ * @see https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+ * @see https://de.wikipedia.org/wiki/Bresenham-Algorithmus
+ */
+template<class t_view, class t_col = typename t_view::value_type,
+	class t_x = typename t_view::x_coord_t, class t_y = typename t_view::y_coord_t>
+void draw_rect(t_view& view, t_x x1, t_y y1, t_x x2, t_y y2, t_col col = t_col{0x00})
+{
+	draw_line(view, x1, y1, x2, y1);
+	draw_line(view, x1, y1, x1, y2);
+	draw_line(view, x2, y2, x1, y2);
+	draw_line(view, x2, y2, x2, y1);
+}
+
+
+/**
+ * bresenham circle algorithm
+ * @see https://de.wikipedia.org/wiki/Bresenham-Algorithmus#Kreisvariante_des_Algorithmus
+ */
+template<class t_view, class t_col = typename t_view::value_type,
+	class t_x = typename t_view::x_coord_t, class t_y = typename t_view::y_coord_t>
+void draw_circle(t_view& view, t_x x_centre, t_y y_centre, t_x rad, t_col col = t_col{0x00})
+{
+	auto draw_all = [x_centre, y_centre, &col, &view](t_x x, t_y y)
+	{
+		view(x_centre + x, y_centre + y) = col;
+		view(x_centre + x, y_centre - y) = col;
+		view(x_centre - x, y_centre + y) = col;
+		view(x_centre - x, y_centre - y) = col;
+	};
+
+
+	t_x x = rad;
+	for(t_y y=0, err=-x; y<x; ++y, err+=2*y + 1)
+	{
+		if(err > 0)
+		{
+			err -= 2*x - 1;
+			--x;
+		}
+
+		draw_all(x, y);
+	}
+
+
+	t_y y = rad;
+	for(t_x x=0, err=-y; x<y; ++x, err+=2*x + 1)
+	{
+		if(err > 0)
+		{
+			err -= 2*y - 1;
+			--y;
+		}
+
+		draw_all(x, y);
 	}
 }
 
@@ -116,12 +202,22 @@ int main()
 			(*iter)[0] = 0xff;
 	}
 
-	draw_line(view, 100, 100, 200, 200);
-	draw_line(view, 100, 200, 200, 100);
-	draw_line(view, 220, 210, 120, 110);
-	draw_line(view, 220, 110, 120, 210);
-	draw_line(view, 50, 50, 50, 100);
-	draw_line(view, 50, 50, 100, 50);
+	double centre_x = 100.;
+	double centre_y = 100.;
+
+	double scale_x = 50.;
+	double scale_y = 50.;
+
+	for(double angle=0.; angle<2.*M_PI; angle+=M_PI/10.)
+	{
+		double s = std::sin(angle);
+		double c = std::cos(angle);
+
+		draw_line(view, centre_x, centre_y, centre_x + scale_x*c, centre_y + scale_y*s);
+	}
+
+	draw_circle(view, 202, 100, 50);
+	draw_rect(view, 48, 48, 254, 152);
 
 	gil::write_view("0.png", view, gil::png_tag{});
 
